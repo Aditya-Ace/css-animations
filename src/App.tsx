00ef58ab20a9adc979499d42babfe3ca77e12ef5
Aditya Tiwari <aditya.ace@hotmail.com>
2024-02-21 09:34:41 +0530

    Initial Files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <div className="container">
+        <div className="earth"></div>
+        <div className="moon"></div>
+      </div>
+      <div className="mars"></div>
+    </main>
+  );
+}
+
+export default App;
